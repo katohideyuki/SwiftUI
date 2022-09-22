43b3bcb0a2f054243a9947fe1bf4cb01b7466977
加藤秀征 <xxwxwxx1@gmail.com>
2022-09-22 22:29:41 +0900

    Section4-2 配列をリスト表示する 1. アクションメニューでListのコードを挿入する 2. 複数の値が入っている配列を作る 3. 配列の値をリストで表示する キーワード : List, \.self, count, indices, ForEach-in, seachable,  filter

--- Ch04/Sec4-2_MetoroList/Sec4-2_MetoroList/ContentView.swift
+++ Ch04/Sec4-2_MetoroList/Sec4-2_MetoroList/ContentView.swift
@@ -9,6 +9,10 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    let FRUITS = ["Apple", "Grape", "Peach", "Banana", "Plum", "Mango", "Yuzu", "Lime", "Lemon"]
+    @State private var searchText = ""
+
     var body: some View {
         /**
          配列をリスト表示する。
@@ -49,6 +53,50 @@ struct ContentView: View {
                 Text(String(index))
                 Text(metoro[index])
             }
+        }
+
+        /**
+         配列から順に要素を取り出す ForEach-in
+         配列から要素を順に取り出したいとき、Viewのbodyではfor-inではなくForEach-inを
+         使うことで配列の要素数を順に取り出すことができる。
+         */
+        VStack {
+            let COLORS: [Color] = [.red, .blue, .green, .orange, .yellow]
+            // 配列の先頭から順に取り出す
+            ForEach(COLORS.indices, id: \.self) { index in
+                // 四角形を描く
+                Rectangle()
+                    .frame(width: 150, height: 30)
+                    .foregroundColor(COLORS[index])
+            }
+        }
+
+        /**
+         配列のフィルタリングを利用する。
+         - 配列のfilter()関数を利用することで、リストに並んでいる値を検索で絞り込んで表示する。
+         検索語句を入力する検索ボックスは、Listのserchable()モディフィアで表示できる。
+         */
+        NavigationView {
+            List {
+                // フィルタリングの結果だけを表示する
+                ForEach(serchResults, id: \.self) { name in
+                    Text(name)
+                }
+            }
+            // 検索ボックスを表示 (「フルーツの検索」は、プレースホルダーとして表示)
+            .searchable(text: $searchText, prompt: "フルーツの検索")
+            .keyboardType(.default)
+            .navigationTitle("人気フルーツ")
+        }
+    }
+
+    // 検索結果を返却する
+    var serchResults: [String] {
+        if searchText.isEmpty {
+            return FRUITS
+        } else {
+            // 配列FRUITSをフィルタリングして得られた配列が変数の値となる
+            return FRUITS.filter { $0.contains(searchText) }
         }
     }
 }
